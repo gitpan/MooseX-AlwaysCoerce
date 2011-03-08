b3f7aff5e4d4a25ffb17f14383e44c64fa73eac8
Karen Etheridge <ether@cpan.org>
2011-03-08 18:35:03 +0000

    Import of ETHER/MooseX-AlwaysCoerce-0.14 from CPAN.
    gitpan-cpan-distribution: MooseX-AlwaysCoerce
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         ETHER/MooseX-AlwaysCoerce-0.14.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/MooseX/AlwaysCoerce.pm
+++ lib/MooseX/AlwaysCoerce.pm
@@ -22,7 +22,7 @@ Version 0.12
 
 =cut
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 =head1 SYNOPSIS
 
@@ -95,8 +95,9 @@ my (undef, undef, $init_meta) = Moose::Exporter->build_import_methods(
     },
 
     role_metaroles => {
-        # applied_attribute should be available soon, for now roles are borked
-        # applied_attribute   => ['MooseX::AlwaysCoerce::Role::Meta::Attribute'],
+        (Moose->VERSION >= 1.9900
+            ? (applied_attribute => ['MooseX::AlwaysCoerce::Role::Meta::Attribute'])
+            : ()),
         role                => ['MooseX::AlwaysCoerce::Role::Meta::Class'],
     }
 );
